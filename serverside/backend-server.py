b168cab884e47fc972f871821f7a2340acf22542
Stephen Cantwell <stephencantwell@n2-65-94.dhcp.drexel.edu>
2014-03-01 21:23:19 -0500

    RAW BUT FINISHED BACKEND

--- serverside/backend-server.py
+++ serverside/backend-server.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, route, run, debug, template, request
+from bottle import Bottle, route, run, debug, template, request, response
 import sqlite3
 from collections import OrderedDict
 
@@ -28,6 +28,19 @@ def get_flight(name):
 	stmt = "SELECT * FROM waypoints WHERE waypoints.FID=( SELECT flightplan_names.FID FROM flightplan_names WHERE flightplan_names.flight_name='" + str(name) + "')"
 	c.execute(stmt)
 	wp = c.fetchall()
+	res = getFlightDetails(name, wp)
+	return res 
+
+# Adds a flight plan to main.db in table flight_plans
+@app.post('/flights/')
+def add_flight():
+
+	response.content_type = 'application/json'
+	response.status = 200
+	updateFlightPlan(request.json)
+
+# Creates json response for a particular flight plan include waypoint array
+def getFlightDetails(name, wp):
 	
 	list=[]
 	res = OrderedDict()
@@ -45,26 +58,7 @@ def get_flight(name):
 		item["deltat"] = wp[i][7]
 		print "ITEM " + str(i) + ": " + str(item)
 		list.append(item)
-
-	return res 
-
-#	return {"name": "2014_02_25_fp1","waypoint":[{"period": 0,"name": "wp1","seq_number": 1,"latitude": 50,"longitude": 60,"altitude": 100,"deltat": 4},{"period": 0,"name": "wp1","seq_number": 1,"latitude": 50,"longitude": 60,"altitude": 100,"deltat": 4}]}
-
-# Adds a flight plan to main.db in table flight_plans
-@app.post('/flights/')
-def add_flight():
-
-#	conn = sqlite3.connect(dbpath)
-#	c = conn.cursor()
-#	c.execute('PRAGMA FOREIGN_KEYS=1')
-#	c.execute('DELETE FROM flightplan_names where fid=6')
-#	conn.commit()
-#	conn.close()
-	return True;
-
-def updatePlans():
-
-	return true
+	return res
 
 
 # Create json for response
@@ -78,5 +72,54 @@ def getFlightNames(res):
 		list.append(item)
 	root["flights"]=list	
 	return root 
+
+# Inserts a new flight plan into the DB
+def updateFlightPlan(res):
+	
+	name = res["name"]
+	wp = res["waypoint"]
+	
+	deleteFlight(name)
+	insertFlightName(name)
+	insertFlightPlan(name, wp)
+
+def insertFlightPlan(name, wp):
+
+	conn = sqlite3.connect(dbpath)
+	c = conn.cursor()
+	stmt = "SELECT FID FROM flightplan_names WHERE flight_name='"+name+"'"
+	c.execute(stmt)
+	res = c.fetchall()
+	fid = res[0][0]
+
+	for row in wp:
+		stmt = "INSERT INTO waypoints (FID, waypoint_name, waypoint_number, latitude, longitude, altitude, delta_time) VALUES ("+str(fid)+",'"+row["name"]+"',"+str(row["seq_number"])+","+str(row["latitude"])+","+str(row["longitude"])+","+str(row["altitude"])+","+str(row["deltat"])+")"
+		c.execute(stmt)
+	conn.commit()
+	c.close()
+
+def insertFlightName(name):
+	
+	conn = sqlite3.connect(dbpath)
+	c = conn.cursor()
+	stmt = "INSERT INTO flightplan_names (flight_name) VALUES ('" + name + "')"
+
+	c.execute(stmt)
+	conn.commit()
+	c.close()
+
+def deleteFlight(name):
+	
+	conn = sqlite3.connect(dbpath)
+	c = conn.cursor()
+	stmt = "SELECT COUNT(*) FROM flightplan_names WHERE flight_name='" + name+"'"	
+	num = c.execute(stmt)
+
+	if num > 0:
+		c.execute('PRAGMA FOREIGN_KEYS=1')
+		c.execute('DELETE FROM flightplan_names where flight_name=\'' + name + '\'')
+		conn.commit()
+		conn.close()
+
 
 app.run(host='localhost', port=8080, debug=True)
